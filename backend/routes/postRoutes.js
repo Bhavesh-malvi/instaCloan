84a9963fef00d1d0a80979696567167b04dbf932
Bhavesh Malviya <bhaveshmalviya@Bhaveshs-MacBook-Air.local>
2026-04-05 16:44:16 +0530

    feat: add getUserPost controller, replace getAllPost with getExplorePosts, and update deletePost authorization status code

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { AuthUser } from "../middleware/authUser.js";
-import { createPost, deletePost, getAllPost, getFeedPost, getPostById, likePost } from "../controllers/postControllers.js";
+import { createPost, deletePost, getExplorePosts, getFeedPost, getPostById, getUserPost, likePost } from "../controllers/postControllers.js";
 import upload from "../middleware/multer.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", AuthUser, upload.array("image", 10), createPost);
 postRouter.post("/like/:postId", AuthUser, likePost);
-postRouter.get("/all", AuthUser, getAllPost);
+postRouter.get("/all", AuthUser, getExplorePosts);
 postRouter.get("/post/:postId", AuthUser, getPostById);
+postRouter.get("/user/:userId", AuthUser, getUserPost);
 postRouter.delete("/delete/:postId", AuthUser, deletePost);
 postRouter.get("/feed", AuthUser, getFeedPost);
 
